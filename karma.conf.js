3cc8e122f77b892d615d2bf0450612f8bada6621
Jacek Bogdanski <jacekbogd@gmail.com>
2020-09-30 09:59:03 +0200

    Refactoring karma config.

--- karma.conf.js
+++ karma.conf.js
@@ -6,30 +6,26 @@ const { join: joinPath } = require( 'path' );
 const basePath = process.cwd();
 const coverageDir = joinPath( basePath, 'coverage' );
 
-module.exports = function(config) {
-	config.set({
+module.exports = function( config ) {
+	config.set( {
 
 		// base path that will be used to resolve all patterns (eg. files, exclude)
 		basePath,
 
-
 		// frameworks to use
 		// available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-		frameworks: ['mocha', 'chai', 'sinon'],
-
+		frameworks: [ 'mocha', 'chai', 'sinon' ],
 
 		// list of files / patterns to load in the browser
 		files: [
 			'tests/**/*.js'
 		],
 
-
 		// preprocess matching files before serving them to the browser
 		// available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
 		preprocessors: {
 			'tests/**/*.js': [ 'webpack' ]
 		},
-
 
 		webpack: {
 			mode: 'development',
@@ -66,7 +62,6 @@ module.exports = function(config) {
 			stats: 'minimal'
 		},
 
-
 		// test results reporter to use
 		// possible values: 'dots', 'progress'
 		// available reporters: https://npmjs.org/browse/keyword/karma-reporter
@@ -74,7 +69,6 @@ module.exports = function(config) {
 			'mocha',
 			'coverage'
 		],
-
 
 		coverageReporter: {
 			reporters: [
@@ -95,28 +89,22 @@ module.exports = function(config) {
 			]
 		},
 
-
 		// web server port
 		port: 9876,
 
-
 		// enable / disable colors in the output (reporters and logs)
 		colors: true,
-
 
 		// level of logging
 		// possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
 		logLevel: config.LOG_INFO,
 
-
 		// enable / disable watching file and executing tests whenever any file changes
 		autoWatch: false,
 
-
 		// start these browsers
 		// available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-		browsers: ['ChromeHeadless'],
-
+		browsers: [ 'ChromeHeadless' ],
 
 		// Continuous Integration mode
 		// if true, Karma captures browsers, runs the tests and exits
@@ -129,5 +117,5 @@ module.exports = function(config) {
 		mochaReporter: {
 			showDiff: true
 		}
-	})
-}
+	} );
+};
